5337fe2a5e491f7507764d090b0bca54c5775c46
Divyansh Garg <divyanshgarg@Divyanshs-MacBook-Air.local>
2025-03-02 18:31:16 +0530

    login api

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1]; // Extract token from `Authorization` header (Bearer token)
+    const token = req.header('Authorization')?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({
@@ -13,9 +13,9 @@ const authenticateToken = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); // Verify the token
-        req.user = decoded; // Attach decoded user information to the request
-        next(); // Proceed to the next middleware or route handler
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = decoded;
+        next();
     } catch (error) {
         res.status(403).json({
             message: 'Invalid token',
