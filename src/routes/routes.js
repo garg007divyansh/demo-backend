8d3a36f98c224db59abad7c2b9297a7567d32d4f
Divyansh Garg <divyanshgarg@Divyanshs-MacBook-Air.local>
2025-03-03 17:50:33 +0530

    optimisation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,10 +10,13 @@ router.get('/getAllRoles', masterController.getAllRoles);
 router.post('/loginUser', authController.loginUser);
 router.post('/create-user', authController.createUser);
 
-// user routes
+// admin routes
 router.get('/getAllUsers', [authenticateToken, checkSuperAdmin], userController.getAllUsers);
 router.get('/getUserById/:id', [authenticateToken, checkSuperAdmin], userController.getUserById);
-router.put('/update-user/:id', authenticateToken, userController.updateUser);
-router.delete('/deleteUserById/:id', authenticateToken, userController.deleteUserById);
+router.put('/update-user/:id', [authenticateToken, checkSuperAdmin], userController.updateUser);
+router.delete('/deleteUserById/:id', [authenticateToken, checkSuperAdmin], userController.deleteUserById);
+
+//user routes
+//get uerby token, update user by toiken
 
 module.exports = router;
